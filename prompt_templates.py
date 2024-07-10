4371d89111195cb2521218e9752804983df88a6b
Ayush Amin <ayush.amin2022@vitstudent.ac.in>
2024-07-10 15:03:02 +0530

    inital commit

---
+++ prompt_templates.py
@@ -0,0 +1,4 @@
+memory_prompt_template = """<>[INST] You are an AI chatbot having a conversation with a human. Answer his questions. If the answere is not present in the context just tell i don't know.
+    Previous conversation: {history}
+    Human: {human_input}
+    AI: [/INST]"""
